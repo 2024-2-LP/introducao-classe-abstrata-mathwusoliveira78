bf1b10083be9097964b84abcbe4f3e274d88f788
mathwusoliveira78 <matheus.tavora@sptech.school>
2024-11-07 18:24:36 -0300

    Subindo exercicios

--- src/main/java/school/sptech/Circulo.java
+++ src/main/java/school/sptech/Circulo.java
@@ -1,4 +1,25 @@
 package school.sptech;
 
-public class Circulo {
+public class Circulo extends Figura {
+    private Double raio;
+
+    @Override
+    public Double calcularArea() {
+        return (raio * raio) * Math.PI;
+    }
+
+    @Override
+    public String toString() {
+        return "Circulo{" +
+                "raio=" + raio +
+                '}';
+    }
+
+    public Double getRaio() {
+        return raio;
+    }
+
+    public void setRaio(Double raio) {
+        this.raio = raio;
+    }
 }
--- src/main/java/school/sptech/Imagem.java
+++ src/main/java/school/sptech/Imagem.java
@@ -1,4 +1,45 @@
 package school.sptech;
 
-public class Imagem {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Imagem  {
+    private List<Figura>figuras;
+
+    public void adicionar(Figura figura) {
+        figuras.add(figura);
+    }
+
+    public Double calcularSomaDasAreas() {
+        Double somaDasAreas = 0.0;
+        for (Figura figura : figuras) {
+            somaDasAreas += figura.calcularArea();
+        }
+        return somaDasAreas;
+    }
+
+    public List<Figura> buscarPorAreaMaiorQue20() {
+        List<Figura>maiorQue20 = new ArrayList<>();
+
+        for (Figura figura : figuras) {
+            if (figura.calcularArea() > 20) {
+                maiorQue20.add(figura);
+            }
+        }
+
+        return maiorQue20;
+    }
+
+    public List<Figura> buscarQuadrados() {
+        List<Figura>quadrado = new ArrayList<>();
+
+        for (Figura figura : figuras) {
+            if(figura instanceof Quadrado) {
+                quadrado.add(figura);
+            }
+        }
+
+        return quadrado;
+    }
+
 }
